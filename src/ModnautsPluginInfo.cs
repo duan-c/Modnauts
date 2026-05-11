5a696e91f798e5fbab9702f4f340013db9b6400c
duan-c <duancoetzee@outlook.com>
2026-05-11 09:04:05 +0200

    plugin guid

--- src/ModnautsPluginInfo.cs
+++ src/ModnautsPluginInfo.cs
@@ -2,7 +2,7 @@
 {
     public static class ModnautsPluginInfo
     {
-        public const string PLUGIN_GUID = "Modnauts";
+        public const string PLUGIN_GUID = "duan-c.Modnauts";
         public const string PLUGIN_NAME = "Modnauts";
         public const string PLUGIN_VERSION = "1.0";
     }
